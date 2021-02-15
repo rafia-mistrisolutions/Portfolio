f3c30000114b217f17929e75a267ca88e17d8c4d
Mahnaz Rafia Islam <52653188+mahnazislam1010@users.noreply.github.com>
2021-02-15 16:00:55 +0600

    first commit

---
+++ js/mainly.js
@@ -0,0 +1,25 @@
+
+
+(function($) {
+
+	"use strict";
+
+   $('.item-wrap a').magnificPopup({
+
+      type:'inline',
+      fixedContentPos: false,
+      removalDelay: 300,
+      showCloseBtn: false,
+      mainClass: 'mfp-fade'
+
+   });
+
+   $(document).on('click', '.popup-modal-dismiss', function (e) {
+   	e.preventDefault();
+   	$.magnificPopup.close();
+   });
+
+	
+
+
+})(jQuery);
